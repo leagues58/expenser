f813f635b29a32a2d33cc6f2310851e9b9d18f53
ericreeves <leagues58@gmail.com>
2018-04-29 23:38:43 -0400

    added db driven store selection dropdown

--- routes/addPurchase.js
+++ routes/addPurchase.js
@@ -1,33 +1,27 @@
 var express = require('express');
-var router = express.Router();
+var router  = express.Router();
+var db      = require('./../db');
+
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
+    
+    db.connection.connect((error) => {
+        if (error) throw error;
+      });
 
-    var storeData = getStoreData();
+    
+    var query = `SELECT
+                    S.store_id, S.name, S.city, S.state
+                FROM Store S`;
 
+    db.connection.query(query, (error, results, fields) => {
+        if (error) throw error;
+        res.render('addPurchase', {storeData: results});
+    });
 
-
-
-
-
-
-
-
-
-
-
-    res.render('addPurchase', {data });
+    
 });
 
 module.exports = router;
-
-
-
-
-
-
-
-function getStoreData() {
-    
-}
